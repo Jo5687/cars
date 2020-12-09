cfebfd463f3cc53392dd57e2393b653a78a5b5d7
Jo5687 <jkaufman@chapman.edu>
2020-12-09 09:25:30 -0800

    Fixed according to assignment

--- cars.sh
+++ cars.sh
@@ -5,15 +5,22 @@ echo "1: Enter a new car"
 echo "2: Display current cars"
 echo "3: Exit program"
 read -r input
- case "$input" in
-  "1") echo "Enter the year"
-       read -r year
-       echo "Enter the make"
-       read -r make
-       echo "Enter the model"
-       read -r model
-       echo "$year":"$make":"$model" >> My_old_cars;;
-  "2") sort -n My_old_cars;;
-  "3") echo "Goodbye!";;
-  *) echo "Sorry, that input was invalid";;
- esac
+while [ "$input" != "3" ]
+do
+ 	case "$input" in
+  		"1") echo "Enter the year"
+       			read -r year
+       			echo "Enter the make"
+       			read -r make
+       			echo "Enter the model"
+       			read -r model
+       			echo "$year":"$make":"$model" >> My_old_cars;;
+  		"2") sort -n My_old_cars;;
+  		"3") echo "Goodbye!";;
+  		*) echo "Sorry, that input was invalid";;
+ 	esac
+	echo "1: Enter a new car"
+        echo "2: Display current cars"
+        echo "3: Exit program"
+        read -r input
+done
