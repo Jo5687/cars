14bbb39c689936b01d3d6317395aafba744a1e0b
Jo5687 <jkaufman@chapman.edu>
2020-12-08 20:22:21 -0800

    Fixed shellcheck errors

--- cars.sh
+++ cars.sh
@@ -4,15 +4,15 @@
 echo "1: Enter a new car"
 echo "2: Display current cars"
 echo "3: Exit program"
-read input
+read -r input
  case "$input" in
   "1") echo "Enter the year"
-       read year
+       read -r year
        echo "Enter the make"
-       read make
-       echo "Enter the model"i
-       read model
-       echo $year:$make:$model >> My_old_cars;;
+       read -r make
+       echo "Enter the model"
+       read -r model
+       echo "$year":"$make":"$model" >> My_old_cars;;
   "2") sort -n My_old_cars;;
   "3") echo "Goodbye!";;
   *) echo "Sorry, that input was invalid";;
